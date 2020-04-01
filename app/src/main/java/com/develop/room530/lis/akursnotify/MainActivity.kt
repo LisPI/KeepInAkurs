b29cd8fa94c591431dec8183ab189e90a1e39298
LisPI <paschka.lis@gmail.com>
2020-04-01 19:08:02 +0300

    feat: add first version Internet checker

--- app/src/main/java/com/develop/room530/lis/akursnotify/MainActivity.kt
+++ app/src/main/java/com/develop/room530/lis/akursnotify/MainActivity.kt
@@ -2,14 +2,18 @@ package com.develop.room530.lis.akursnotify
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 
 const val KEY_CURRENCY = "key_currency"
 const val KEY_CURRENCYNB = "key_currencyNB"
@@ -18,10 +22,6 @@ class MainActivity : AppCompatActivity() {
 
     private var currency = MutableLiveData<String>()
     private var currencyNB = MutableLiveData<String>()
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
-    }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
@@ -52,6 +52,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getCurrency(){
+
+        if (checkInternet() != true) {
+            currency.value = "Check your Internet connection"
+            currencyNB.value = ""
+            return
+        }
+
         CoroutineScope(Dispatchers.Main).launch {
             val doc = withContext(Dispatchers.IO) {
                 Jsoup.connect("https://www.alfabank.by/services/a-kurs/").get()
@@ -59,5 +66,11 @@ class MainActivity : AppCompatActivity() {
             currency.value = doc.select(".curr-table tr:first-of-type td:first-of-type").text()
             currencyNB.value = getString(R.string.NB) + " : " + doc.select(".curr-table tr:last-of-type td:first-of-type").text()
         }
+    }
+
+    private fun checkInternet(): Boolean? {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting
     }
 }
