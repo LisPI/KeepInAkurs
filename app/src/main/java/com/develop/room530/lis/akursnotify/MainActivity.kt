6554980a3fcc820a2908b90744db43c6347c0444
LisPI <paschka.lis@gmail.com>
2021-05-03 23:06:45 +0300

    play with styles

--- app/src/main/java/com/develop/room530/lis/akursnotify/MainActivity.kt
+++ app/src/main/java/com/develop/room530/lis/akursnotify/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
